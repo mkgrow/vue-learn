29955c3d4dc056b422c6b23f2fc0400fdf936eb8
makan <makan@sevencent.com.cn>
2018-01-26 10:01:43 +0800

    修改发现页面icon

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,15 +3,15 @@
     <mt-header fixed :title="title" class="title"></mt-header>
     <div class="item">
       <mt-cell title="我的收藏" is-link>
-        <img slot="icon" src="../assets/contact/my-favorite.svg">
+        <img slot="icon" src="../assets/contact/my-favorite.svg" class="icon">
       </mt-cell>
     </div>
     <div>
       <mt-cell title="好友车" is-link>
-        <img slot="icon" src="../assets/contact/car-friends.svg">
+        <img slot="icon" src="../assets/contact/car-friends.svg" class="icon">
       </mt-cell>
       <mt-cell title="应用号" is-link>
-        <img slot="icon" src="../assets/contact/app-account.svg">
+        <img slot="icon" src="../assets/contact/app-account.svg" class="icon">
       </mt-cell>
     </div>
   </div>
@@ -38,5 +38,9 @@
 
   .item {
     margin: 12px 0;
+  }
+
+  .icon {
+    border-radius: 5px;
   }
 </style>
